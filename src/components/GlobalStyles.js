a11ee5254e3d0d90f4dcdf2b07253beb99b0a7c0
John Dimm <john.dimm@gardyn.tech>
2024-04-12 09:04:54 -0700

    Better yellow title.

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -36,8 +36,9 @@ const GlobalStyles = () => {
 .fp_filter_panel {
 	width: 250px;
 	float: left;
-	padding: 10px;
-	border: 1px solid gray;
+	padding-left: 10px;
+	padding-right: 10px;
+
 	margin-bottom: 100px;
 	height: 100%;
 	overflow: auto;
@@ -114,8 +115,13 @@ const GlobalStyles = () => {
 }
 
 @font-face {
-    font-family: komika;
+    font-family: komikab;
     src: url(KOMIKAHB.ttf);
+}
+
+@font-face {
+    font-family: komika;
+    src: url(KOMIKAH_.ttf);
 }
 
 .page_title {
@@ -130,56 +136,19 @@ const GlobalStyles = () => {
 
 .page_subtitle {
 	font-size: 10pt;
-
 	text-align: center;
-	margin-bottom: 15px;
 	font-family: komika;
 	padding: 5px;
 	background-color: yellow;
 	border: 2px solid darkblue;
-	width: 220px;
-	height: 50px;
-}
-
-.dummy {
-	border-radius:10px;
-}
-
-.xtitles {
-
-
-}
-
-$clr-primary: #f7cfc8;
-$clr-secondary: #4d84c8;
-$clr-wht: #fffcf6;
-
-.chunky_page_title {
-	color: $clr-primary;
-	font-family: "Luckiest Guy";
-	font-size: 22px;
-	letter-spacing: 2px;
-	text-align: center;
-	text-shadow: -6px -6px $clr-secondary, -5px -5px $clr-secondary,
-	  -4px -4px $clr-secondary, -3px -3px $clr-secondary, -2px -2px $clr-secondary,
-	  -1px -1px $clr-secondary, 1px 1px $clr-secondary, 2px 2px $clr-secondary,
-	  3px 3px $clr-secondary, 4px 4px $clr-secondary, 5px 5px $clr-secondary,
-	  6px 6px $clr-secondary, 7px 7px $clr-secondary, 8px 8px $clr-secondary,
-	  9px 9px $clr-secondary, 10px 10px $clr-secondary, 11px 11px $clr-secondary,
-	  12px 12px $clr-secondary, 13px 13px $clr-secondary, 14px 14px $clr-secondary,
-	  15px 15px $clr-secondary, 16px 16px $clr-secondary, 17px 17px $clr-secondary,
-	  18px 18px $clr-secondary, 19px 19px $clr-secondary, 20px 20px $clr-secondary,
-	  21px 21px $clr-secondary, 22px 22px $clr-secondary, 23px 23px $clr-secondary,
-	  24px 24px $clr-secondary, 25px 25px $clr-secondary, 26px 26px $clr-secondary,
-	  27px 27px $clr-secondary, 25px 25px $clr-secondary, 28px 28px $clr-secondary,
-	  29px 29px $clr-secondary, 30px 30px $clr-secondary, 31px 31px $clr-secondary,
-	  32px 32px $clr-secondary, 33px 33px $clr-secondary, 34px 34px $clr-secondary,
-	  35px 35px $clr-secondary, 36px 36px $clr-secondary, 37px 37px $clr-secondary,
-	  38px 38px $clr-secondary, 39px 39px $clr-secondary, 40px 40px;
 	width: 100%;
-  }
-  
+	margin-bottom: 10px;
 
+}
+
+.marvel_comics {
+	font-family: komikab;
+}
 
 
 dt {
